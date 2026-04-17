ffb6832edd94431169e8243d8b329cb06583a1bc
salaberry10 <josemsalaberry@hotmail.com>
2026-04-17 14:41:04 +0200

    feat: página inicio básica

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Teno Fisioterapia</title>
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+<body>
+    <h1>Bienvenido a Teno Fisioterapia</h1>
+</body>
+</html>
